c2cc1072ef040f0a1de27d5563af16bee201a55c
Sourav Kumar <souravkumar4297@gmail.com>
2023-05-16 18:40:26 +0530

    Project basic setup

---
+++ E-commerce/my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Header } from "./frontend/components/Header";
+// import Mockman from "mockman-js";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ram</h1>
+      <Header />
+      {/* <Mockman /> */}
+    </div>
+  );
+}
+
+export default App;
